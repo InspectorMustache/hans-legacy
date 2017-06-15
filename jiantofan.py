4c886f147ab062ee595b16ca194d8a7318ae2f26
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-06-16 00:17:59 +0200

    Turn CR object into something a little more useful

--- jiantofan.py
+++ jiantofan.py
@@ -1,54 +1,35 @@
-from decomposer import Decompose
-
-
 class ComponentRelation(object):
 
     def __init__(self,
-                 jian_component,
-                 fan_component,
-                 jian_pool,
                  mappings,
-                 **kwargs):
-        self.jian_component = jian_component
-        self.fan_component = fan_component
-        self.jian_pool = jian_pool
-        self.jian_mappings = mappings.jians
-        self.fan_mappings = mappings.fans
-        if not fan_pool in kwargs:
-            self.fan_pool = self.mappings.fans.keys()
-        else:
-            self.fan_pool = fan_pool
-        self.jian_list = self.get_list_by_component(jian_component, jian_pool)
-        self.fan_list = self.get_list_by_component(fan_component, fan_pool)
+                 comp_dict):
+        self.jian_pool = mappings.jians.keys()
+        self.fan_pool = mappings.fans.keys()
+        self.mappings = mappings
+        self.comp_dict = comp_dict
 
-    def get_list_by_component(self, component, pool):
-        '''Get a list of all Hanzi in $pool that contain $component.'''
+    def get_chars(self, component, charset=None):
+        '''Return all Hanzi that contain component'''
         component_list = []
+
+        charset = charset or 'jian'
+        if charset == 'jian':
+            pool = self.jian_pool
+        elif charset == 'fan':
+            pool = self.fan_pool
+
         for char in pool:
-            print('Processing {}...'.format(char))
-            if component in Decompose(char).break_down():
+            if component in self.comp_dict[char]:
                 component_list.append(char)
+        print('Done.')
         return component_list
 
-    def get_pairs(self, *args):
-        pair_list = []
-        if 'jian' in args:
-            for char in self.jian_list:
-                pair_list.append(self.make_pair_dict(char,
-                                                     self.jian_mappings[char]))
-        elif 'fan' in args:
-            for char in self.fan_list:
-                pair_list.append(self.make_pair_dict(char,
-                                                     self.fan_mappings[char]))
-
-    # def connect_pairs(self, chars, opposite_chars, mapping):
-    #     pair_list = []
-    #     for char in char_list:
-    #         opposite = mapping[char]
-    #         if opposite in opposite_chars
-
-    def make_pair_dict(self, jian, fan):
+    def get_pairs(self, char, charset=None):
+        '''Return matching chars from the other charset.'''
         pair_dict = {}
-        pair_dict['jian'] = jian
-        pair_dict['fan'] = fan
-        return pair_dict
+        mappings = self.mappings
+        charset = charset or 'jian'
+        if charset == 'jian':
+            pair_dict[char] = mappings.jians[char]
+        elif charset == 'fan':
+            pair_dict[char] = mappings.fans[char]
