b22ca55a3b1d47f75cda9a5e296934dcbd91cdb2
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-03-19 21:07:56 +0100

    Simplify lessons output

--- lessons.py
+++ lessons.py
@@ -1,7 +1,6 @@
 import textwrap
 import os
 import re
-
 learned_chars = {}
 
 if not os.path.exists('lessons'):
@@ -27,18 +26,19 @@ class LessonWriter(object):
     def __enter__(self):
         return self
 
-    def get_charmap_line(self, jian, fan, indent='', arrow='-->'):
-        line = '{}J[{}] {} F[{}]'.format(indent, jian, arrow, fan)
+    def get_charmap_line(self, jian, fan, rule=None, indent='', arrow='-->'):
+        rule_frag = ' !{}'.format(rule) if rule else ''
+        line = 'J{}<{}> {} F<{}>{}'.format(indent, jian, arrow, fan, rule_frag)
         return line
 
-    def get_compmap_line(self, jian, fan, indent='', arrow='-->'):
-        line = 'J{}<{}> {} F<{}>'.format(indent, jian, arrow, fan)
+    def get_compmap_line(self, jian, fan, rule=None, indent='', arrow='-->'):
+        rule_frag = ' !{}'.format(rule) if rule else ''
+        line = 'J{}<{}> {} F<{}>{}'.format(indent, jian, arrow, fan, rule_frag)
         return line
 
-    def append_charmap_as_lines(self, rules, indent='\t', arrow='-->',
-                                terminator=None):
+    def append_charmap_as_lines(self, rules, terminator=None, **kwargs):
         for jian, fan in rules.items():
-            self.lines.append(self.get_charmap_line(jian, fan, indent='\t'))
+            self.lines.append(self.get_charmap_line(jian, fan, **kwargs))
         if type(terminator) is str:
             self.lines.append(terminator)
 
@@ -48,7 +48,7 @@ class LessonWriter(object):
         underlying char rule, than the resulting char rules. Also update
         learned_chars."""
         learned_chars.update({jian: fan})
-        self.lines.append(self.get_charmap_line(jian, fan))
+        self.lines.append(self.get_charmap_line(jian, fan, comp_type))
         rules = self.crel.get_learnables(jian, fan, learned_chars, comp_type)
         if rules is None:
             pass
@@ -56,13 +56,14 @@ class LessonWriter(object):
             learned_chars.update(rules)
             self.append_charmap_as_lines(rules, **kwargs)
 
-    def append_comp_rule_block(self, jian, fan, learned_chars, **kwargs):
+    def append_comp_rule_block(self, jian, fan,
+                               learned_chars, comp_type=None, **kwargs):
         """Get a list of lines consisting of a block of rules: First, the
         underlying comp rules, than the resulting char rules. Also update
         learned_chars."""
         learned_chars.update({jian: fan})
-        self.lines.append(self.get_compmap_line(jian, fan))
-        rules = self.crel.get_learnables(jian, fan, learned_chars)
+        self.lines.append(self.get_compmap_line(jian, fan, comp_type))
+        rules = self.crel.get_learnables(jian, fan, learned_chars, comp_type)
         if rules is None:
             pass
         else:
@@ -89,143 +90,43 @@ def create_lessons(crel):
 
 def lesson_1(crel):
     with LessonWriter('lesson1', crel) as lesson:
-        lesson.append_comment('''This is the first lesson and by all
-                probability also the largest. It contains all those simplified
-                characters, of which you already know the traditional variant,
-                meaning they are identical. These characters are not ordered in
-                any meaningful way so this lesson mainly serves to give you a
-                basic overview of the number of characters you will not have to
-                learn anew.''', terminator='')
-        lesson.append_comment('''This lesson also establishes the basic syntax
-                of this and all following lessons: A character is indicated by
-                surrounding square brackets, while square brackets are preceded
-                by either J or F, signaling a simplified (J:简体字) or
-                traditional character (F:繁體字).''', terminator='')
-
         rules = {}
         for jian in crel.jian_pool:
             if crel.test_identical_fan(jian):
                 rules[jian] = crel.get_correspondence(jian)[0]
-
         learned_chars.update(rules)
         lesson.append_charmap_as_lines(rules)
 
 
 def lesson_2(crel):
     with LessonWriter('lesson2', crel) as lesson:
-        lesson.append_comment('''This first rule is more of a variant writing
-                (not a variant character). Thus, depending on the font you're
-                using, you might not actually see any difference between the
-                characters below.  However, simplified and traditional
-                characters that only differ with regards to this component,
-                still occupy different unicode codepoints.  So it's good to get
-                this rule out of the way early on. The simplified character set
-                unified this component to 兑, that is the variant with the two
-                strokes on the top pointing inward. For traditional characters
-                however, both variants are acceptable. From what I can tell,
-                the one with the strokes pointing inward is more common in
-                writing, while the other (兌) is more prevalent in printed
-                text. It's probably sufficient to stick with the version you
-                know from the simplified character set.''')
-        lesson.append_comp_rule_block('兌', '兌',
-                                      learned_chars,
-                                      terminator='\n')
-
-        lesson.append_comment('''These next three rules should also be rather
-                easy to aquire. There are no exceptions to these rules, they
-                cover a lot of characters and the components always occur in
-                the same position, that is on the left of anotheer component.
-                You should also already be familiar with the traditional form
-                of the components as they exist as individual characters in
-                both character sets with no differences.''', terminator='')
-
-        lesson.append_comment('''The syntax for component rules differs from
-                that for character rules. Angle brackets preceded by either J
-                or F denote simplified or traditional components but they don't
-                imply identical rules for corresponding characters.''')
-        lesson.append_comment('''So J<讠> --> F<言> doesn't mean J[讠] -->
-                F[言].''', terminator='')
-
-        lesson.append_comp_rule_block('讠', '言', learned_chars, terminator='')
-
+        lesson.append_comp_rule_block('兌', '兌', learned_chars, terminator='')
         lesson.append_comp_rule_block('钅', '金', learned_chars, terminator='')
-
         lesson.append_comp_rule_block('饣', '食', learned_chars, terminator='')
-
-        lesson.append_comment('''The traditional variant of this one does not
-                appear in the simplified charset, however it occurs as commonly
-                and is as consistent in its positioning as the other three.''')
         lesson.append_comp_rule_block('纟', '糹', learned_chars)
 
 
 def lesson_3(crel):
     with LessonWriter('lesson3', crel) as lesson:
-        lesson.append_comment('''The rules of this chapter concern characters
-                that appear in more diverse positions than those of the last
-                lesson but are still rather easy to learn.''', terminator='')
-        lesson.append_comment('''Since you are most likely familiar with the
-                character "目", this first rule should be simple to learn.''')
-
         lesson.append_char_rule_block('贝', '貝', learned_chars, terminator='')
-
-        lesson.append_comment('''This is almost the same as the above rule with
-                the exception of the right stroke at the bottom.''')
         lesson.append_char_rule_block('见', '見', learned_chars, terminator='')
-
         # lesson.append_char_rule_block('丬', '爿', learned_chars, terminator='')
-
         lesson.append_char_rule_block('争', '爭', learned_chars, terminator='')
-
         # lesson.append_char_rule_block('单', '單', learned_chars, terminator='')
-
-        lesson.append_comment('''Yes, the "simplification" in this case consists of
-                removing the one short line between the two 口.''')
         lesson.append_char_rule_block('吕', '呂', learned_chars, terminator='')
-
-        lesson.append_comment('''艹 and 炏 in the following rule don't apply to
-                these components in general, but only when they appear on top
-                of 冖. However, no unicode character exists for these two
-                specific component. That is why this rule is represented by
-                components lacking the 冖 part. ''')
-        lesson.append_char_rule_block('艹', '炏', learned_chars, comp_type='冖')
+        lesson.append_comp_rule_block('艹', '炏', learned_chars, comp_type='冖')
 
 
 def lesson_4(crel):
     with LessonWriter('lesson4', crel) as lesson:
-        lesson.append_comment('''The rules in this lesson concern traditional
-        characters that are more complex than their simplified counterparts -
-        in some cases significantly so, however they have an iconographic
-        character to them. Thus, they shouldn't be too difficult to learn.''')
-        lesson.append_comment('''The first rules mostly cover characters
-        depicting different types of animals. For consistency's sake [鱼] -->
-        [魚] is listed here as well, even though its simplicity fits better
-        with the rules of the last lesson.''')
-
         lesson.append_char_rule_block('鱼', '魚', learned_chars, terminator='')
-
         lesson.append_char_rule_block('马', '馬', learned_chars, terminator='')
-
         lesson.append_char_rule_block('鸟', '鳥', learned_chars, terminator='')
-
-        lesson.append_comment('''This character appears rarely as a component.
-        However, it makes sense to learn it together with the last one.''')
         lesson.append_char_rule_block('乌', '烏', learned_chars, terminator='')
-
         lesson.append_char_rule_block('龙', '龍', learned_chars, terminator='')
-
         lesson.append_char_rule_block('黾', '黽', learned_chars, terminator='')
-
-        lesson.append_comment('''Another rare character and what's more, one
-        that is quite difficult to draw. Nevertheless, in a lesson concerning
-        different animal related characters, it needs to be mentioned as
-        well.''')
-        lesson.append_char_rule_block('龟', '龜', learned_chars, terminator='\n')
-
-        lesson.append_comment('''While these last rules don't refer to animals,
-        they also cover characters which have an iconographic quality.''')
+        lesson.append_char_rule_block('龟', '龜', learned_chars, terminator='')
         lesson.append_char_rule_block('东', '東', learned_chars, terminator='')
-
         # what's going on here?
         lesson.append_comp_rule_block('柬', '東', learned_chars, terminator='')
-
         lesson.append_comp_rule_block('车', '車', learned_chars)
